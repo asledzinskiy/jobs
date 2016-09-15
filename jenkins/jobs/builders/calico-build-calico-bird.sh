a4255fffa86243257d705572d5eae97b59048674
Sergey Kulanov <skulanov@mirantis.com>
2016-09-15 11:35:10 +0300

    Move calico-bird build job to separate template
    calico-bird doesn't depend on any calico components so we need
    to have separate flow for it, since in the next step it should
    trigger build container job.
    We also move building script from calico-bird project code
    to job
    
    Change-Id: I7ccb81293b2e0b56fbbc8f610257d258d7aafa85

---
+++ jenkins/jobs/builders/calico-build-calico-bird.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+BUILD=$(git rev-parse --short HEAD)
+
+rm -rf ./artifacts/
+
+/bin/sh -x build.sh
+
+mkdir -p ./artifacts/
+
+# Prepare artifacts
+cp ./dist/bird ./artifacts/bird-"${BUILD}"
+cp ./dist/bird6 ./artifacts/bird6-"${BUILD}"
+cp ./dist/birdcl ./artifacts/birdcl-"${BUILD}"
+rm -rf ./dist
+echo "${BUILD}" > ./artifacts/lastbuild
