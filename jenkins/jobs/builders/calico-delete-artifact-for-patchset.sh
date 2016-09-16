93b21718297c07755a2c33c29e9ee112d6e213f7
Sergey Kulanov <skulanov@mirantis.com>
2016-09-16 11:03:07 +0300

    Update delete artifacts logic
    We shouldn't try to delete artifacts if
    GERRIT_CHANGE_NUMBER is not defined, otherwise we can have
    incorrect path to artifacts that should be removed
    
    Change-Id: Id46999724026a058fc0aea54b8c228fc0e456249

--- jenkins/jobs/builders/calico-delete-artifact-for-patchset.sh
+++ jenkins/jobs/builders/calico-delete-artifact-for-patchset.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
+# do not perform any action if GERRIT_CHANGE_NUMBER is not defined
+[ -z "${GERRIT_CHANGE_NUMBER}" ] && exit 0
+
 # delete merged artifact
 curl -u "${ARTIFACTORY_LOGIN}:${ARTIFACTORY_PASSWORD}" -X DELETE "${ARTIFACTORY_URL}/${GERRIT_CHANGE_NUMBER}" || true
