02469afd2a45c553438b24edc91a7e5a6a40842a
Alexander Saprykin <asaprykin@mirantis.com>
2016-09-22 15:04:21 +0200

    Build IPAM plguin for calico
    Change-Id: I9ea0cd591067cb71af33f573dade9588330c1beb

--- jenkins/jobs/builders/calico-build-calico-cni.sh
+++ jenkins/jobs/builders/calico-build-calico-cni.sh
@@ -19,17 +19,18 @@ rm -rf "${WORKSPACE}/dist" \
 docker run --rm \
     -v ${WORKSPACE}:/code \
     ${LIBCALICO_DOCKER_IMAGE} \
-    /bin/sh -c "pip install pykube && pyinstaller calico.py -ayF"
+    /bin/sh -c "pip install pykube && pyinstaller calico.py -ayF && pyinstaller ipam.py -ayF -n calico-ipam"
 
 mkdir -p "${WORKSPACE}/artifacts"
 cp "${WORKSPACE}/dist/calico" "${WORKSPACE}/artifacts/calico-${BUILD}"
+cp "${WORKSPACE}/dist/calico-ipam" "${WORKSPACE}/artifacts/calico-ipam-${BUILD}"
 
 # Create config yaml for Kargo
 cat > ./artifacts/calico-cni-${BUILD}.yaml << EOF
 calico_cni_download_url: ${ARTIFACTORY_URL}/calico-cni/calico-${BUILD}
 calico_cni_checksum: $(sha256sum ${WORKSPACE}/artifacts/calico-${BUILD} | awk '{ print $1 }')
-calico_cni_ipam_download_url: ${ARTIFACTORY_URL}/calico-cni/calico-${BUILD}
-calico_cni_ipam_checksum: $(sha256sum ${WORKSPACE}/artifacts/calico-${BUILD} | awk '{ print $1 }')
+calico_cni_ipam_download_url: ${ARTIFACTORY_URL}/calico-cni/calico-ipam-${BUILD}
+calico_cni_ipam_checksum: $(sha256sum ${WORKSPACE}/artifacts/calico-ipam-${BUILD} | awk '{ print $1 }')
 EOF
 
 echo "${BUILD}" > ./artifacts/lastbuild
