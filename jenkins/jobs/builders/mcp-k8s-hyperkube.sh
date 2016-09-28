b147d069cd2260b3d4cde62e3da8ca6fa7816f1e
Artem Panchenko <apanchenko@mirantis.com>
2016-09-28 16:54:53 +0300

    Execute MCP integration tests for hyperkube
    Run MCP deployment tests with Calico and network
    verification for each patch to the hyperkube project.
    
    Voting is temporarily disabled.
    
    Change-Id: I8fed4a484fb9ffd934fb23c3f9e827ef43bd5e55

--- jenkins/jobs/builders/mcp-k8s-hyperkube.sh
+++ jenkins/jobs/builders/mcp-k8s-hyperkube.sh
@@ -7,6 +7,38 @@ if [[ -z "${KUBE_DOCKER_OWNER:-}" ]]; then
   exit -1
 fi
 
+wait_for_artifacts () {
+    local VERSION="$1"
+    local TIMEOUT="${2:-1200}"
+    local INTERVAL="${3:-5}"
+    local DEADLINE=$(($(date +%s)+${TIMEOUT}))
+    local ARTIFACTS_YAML="hyperkube_image_${VERSION}.yaml"
+    local ARTIFACTS_URL="${ARTIFACTORY_URL}/mcp-k8s-ci/images-info/${ARTIFACTS_YAML}"
+
+    while [[ "$(date +%s)" -lt "${DEADLINE}" ]]; do
+        local HTTP_CODE=$(curl -sw '%{http_code}' -o /dev/null -I "${ARTIFACTS_URL}")
+        if [[ "${HTTP_CODE}" -eq 200 ]]; then
+            wget -N "${ARTIFACTS_URL}"
+            echo "${ARTIFACTS_YAML}"
+            return 0
+        else
+            sleep "${INTERVAL}"
+        fi
+    done
+    echo 1>&2 "ERROR! Time is out while waiting for artifacts: ${ARTIFACTS_YAML}!"
+    exit 1
+}
+
+save_tests_params () {
+    local SOURCE_YAML="$1"
+    local RESULT_PROPERTIES_FILE="$2"
+    python2 > "${RESULT_PROPERTIES_FILE}" <<EOF
+import yaml
+for k,v in yaml.load(open("${SOURCE_YAML}")).items():
+    print '{0}={1}'.format(k.upper(), v)
+EOF
+}
+
 export REGISTRY="${KUBE_DOCKER_REGISTRY}/${KUBE_DOCKER_OWNER}"
 
 CALICO_CNI="${CALICO_CNI:-https://github.com/projectcalico/calico-cni/releases/download/v1.3.1/calico}"
@@ -35,6 +67,27 @@ $(git pull kubernetes; echo true)
 git fetch ssh://mcp-ci-gerrit@"${GERRIT_HOST}":29418/"${GERRIT_PROJECT}" "${GERRIT_REFSPEC}" && git checkout FETCH_HEAD
 
 export GIT_COMMIT_TAG_ID=$(git describe --tags --abbrev=4)
+export KUBE_DOCKER_VERSION="${GIT_COMMIT_TAG_ID}_${BUILD_NUMBER}"
+export VERSION="${KUBE_DOCKER_VERSION}"
+
+# if this script is executed from tests job - don't build anything,
+# just wait until artifacts appear in the artifactory
+if [[ "${ONLY_WAIT_FOR_ARTIFACTS}" == "true" ]]; then
+    if [ -z "${HYPERKUBE_ARTIFACTS_FILE}" ]; then
+        echo 1>&2 \
+        "ERROR! The variable HYPERKUBE_ARTIFACTS_FILE for test properties file isn't set!"
+        exit 1
+    fi
+    if [[ -z "${GERRIT_CHANGE_NUMBER}" || -z "${GERRIT_PATCHSET_NUMBER}" ]]; then
+        echo 1>&2 \
+        "ERROR! Can't find artifacts for tests! Looks like the build wasn't triggered by gerrit event!"
+        exit 1
+    fi
+    PATCHSET_VERSION="${GIT_COMMIT_TAG_ID}_${GERRIT_CHANGE_NUMBER}+${GERRIT_PATCHSET_NUMBER}"
+    ARTIFACTS_YAML=$(wait_for_artifacts "${PATCHSET_VERSION}" 1200)
+    save_tests_params "${WORKSPACE}/${ARTIFACTS_YAML}" "${HYPERKUBE_ARTIFACTS_FILE}"
+    exit 0
+fi
 
 cat <<EOF > "${WORKSPACE}/build.sh"
 #!/bin/bash
@@ -45,9 +98,6 @@ kube::build::run_build_command hack/build-go.sh cmd/hyperkube
 EOF
 chmod +x "${WORKSPACE}/build.sh"
 sudo -E -s "${WORKSPACE}/build.sh"
-
-export KUBE_DOCKER_VERSION="${GIT_COMMIT_TAG_ID}_${BUILD_NUMBER}"
-export VERSION="${KUBE_DOCKER_VERSION}"
 
 # Inject build info to the image
 pushd "${WORKSPACE}/cluster/images/hyperkube"
@@ -114,3 +164,10 @@ hyperkube_image_repo: "${KUBE_DOCKER_REGISTRY}/${KUBE_DOCKER_REPOSITORY}"
 hyperkube_image_tag: "${KUBE_DOCKER_VERSION}"
 gerrit_change_url: "${GERRIT_CHANGE_URL}"
 EOF
+
+# copy versions of images to artifact for patchset
+if [[ -n "${GERRIT_CHANGE_NUMBER}" && -n "${GERRIT_PATCHSET_NUMBER}" ]]; then
+    PATCHSET_VERSION="${GIT_COMMIT_TAG_ID}_${GERRIT_CHANGE_NUMBER}+${GERRIT_PATCHSET_NUMBER}"
+    cp "${WORKSPACE}/hyperkube_image_${VERSION}.yaml" \
+       "${WORKSPACE}/hyperkube_image_${PATCHSET_VERSION}.yaml"
+fi
