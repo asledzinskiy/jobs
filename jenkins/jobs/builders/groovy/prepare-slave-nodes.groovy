9b53cbbbeb433b9e4406c3ac2aa0b75afb7ef883
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-10-06 15:28:14 +0200

    Move prepare-slave-nodes job to pipelines
    This change migrates the prepare-slave-nodes
    to the pipelines workflow. Also it will use
    the init-config feature of a mcp-ci.sh script,
    to generate the initial, generic, config.
    
    Change-Id: Ic9d525e7376bfc30cd675f5fb35c327905426d0b

---
+++ jenkins/jobs/builders/groovy/prepare-slave-nodes.groovy
@@ -0,0 +1,60 @@
+node('tools') {
+  try {
+
+    def TEST_MODE = "True"
+    def VENV_PATH = "${WORKSPACE}/.tox/mcp-ci"
+    def ANSIBLE_HOST_KEY_CHECKING="False"
+    def INVENTORY = "${WORKSPACE}/conf/slave_nodes_inventory"
+    def HOSTS = "${HOSTS_LIST}"
+    def USER = "${USERNAME}"
+    def BRANCH = "${BRANCH_NAME}"
+    def HOSTNAME = sh(returnStdout: true, script: "hostname").trim()
+    ArrayList HOST = new ArrayList(Arrays.asList(HOSTS.split("\\s* \\s*")));
+
+    stage ('Git checkout') {
+      gitSSHCheckout {
+        credentialsId = "mcp-ci-gerrit"
+        branch = BRANCH
+        host = "review.fuel-infra.org"
+        project = "mcp-ci/mcp-cicd-poc"
+      }
+    }
+
+    stage('Construct the inventory') {
+      def str = "[slave-nodes]\n"
+      for ( int i = 0; i < HOST.size(); i++ ) {
+        if (HOST.get(i) == "localhost" || HOST.get(i) =~ /^127\./ || HOST.get(i) =~ /^${HOSTNAME}.*/) {
+          println "localhost detected - skipping!"
+        } else {
+          str += HOST.get(i) + " ansible_user=" + USER + " ansible_connection=ssh\n"
+        }
+      }
+      writeFile file: INVENTORY, text: "${str}"
+    }
+
+    withEnv(["VENV_PATH=${VENV_PATH}",
+             "TEST_MODE=${TEST_MODE}",
+             "ANSIBLE_HOST_KEY_CHECKING=${ANSIBLE_HOST_KEY_CHECKING}" ]) {
+      stage ('Create VENV') {
+        sh "tox -e mcp-ci"
+      }
+
+      stage ('Create initial config') {
+        sh "./mcp-ci.sh init-config"
+      }
+
+      stage ('Execute prepare-slave-nodes playbook') {
+        sshagent(['jenkins']) {
+          sh "./mcp-ci.sh prepare-slave-nodes"
+        }
+      }
+    }
+
+  }
+  catch(err) {
+    echo "Failed: ${err}"
+    currentBuild.result = 'FAILURE'
+  }
+  finally {
+  }
+}
