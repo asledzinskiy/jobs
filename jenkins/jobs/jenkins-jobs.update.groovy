344b3fb98ccfec12fe81b4985ae4a21c9daafe43
Artur Kaszuba <akaszuba@mirantis.com>
2016-10-07 12:32:08 +0200

    Switch jenkins-job update to pipeline
    Change-Id: Ib472f55561b74b8f09042ed07fd83aee7689e605

---
+++ jenkins/jobs/jenkins-jobs.update.groovy
@@ -0,0 +1,43 @@
+node('tools') {
+
+    stage('Code checkout') {
+        def HOST = env.GERRIT_HOST
+        gitSSHCheckout {
+            credentialsId = "mcp-ci-gerrit"
+            branch = "master"
+            host = HOST
+            project = "mcp-ci/project-config"
+        }
+    }
+
+    stage('JJB update') {
+        try {
+            withCredentials([[
+                    $class: 'UsernamePasswordMultiBinding',
+                    credentialsId: 'jjb-updater',
+                    usernameVariable: 'JJB_USER',
+                    passwordVariable: 'JJB_PASS']]) {
+                writeFile file: "${WORKSPACE}/jenkins_jobs.ini", text: """\
+                    [jenkins]
+                    user=${env.JJB_USER}
+                    password=${env.JJB_PASS}
+                    url=${env.JENKINS_URL}
+                    [job_builder]
+                    ignore_cache=True
+                    recursive=True
+                    """.stripIndent()
+            }
+            sh 'tox -e mcp-ci -r'
+            sh """${WORKSPACE}/.tox/mcp-ci/bin/jenkins-jobs \
+                    --flush-cache \
+                    --conf ${WORKSPACE}/jenkins_jobs.ini \
+                    update --delete-old jenkins/jobs"""
+        } catch(err) {
+            echo "Failed: ${err}"
+            currentBuild.result = 'FAILURE'
+        } finally {
+            sh "rm -f ${WORKSPACE}/jenkins_jobs.ini"
+        }
+    }
+
+}
