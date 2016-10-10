6f3a5ddf7f8f78083e650148822c1b24bbded046
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-10-10 15:12:54 +0200

    Migrated mcp-prepare-env to the pipelines
    This changes migrates mcp-prepare-env job
    to the pipelines workflow.
    
    Change-Id: Ic2660fbaef1f88f51ca5910ee3843e02470ed75f

---
+++ jenkins/jobs/builders/groovy/mcp-prepare-env.groovy
@@ -0,0 +1,78 @@
+node('tools') {
+
+  def DELETE_DB = "${DELETE_DB}"
+  def VIRTUAL_ENV = "/home/jenkins/venv-fuel-devops-3.0"
+  def LOG_FILE = "/home/jenkins/.devops/log.yaml"
+  def DEVOPS_DB_ENGINE = "django.db.backends.sqlite3"
+  def DEVOPS_DB_NAME = "/home/jenkins/venv-fuel-devops-3.0.sqlite3.db"
+  def VENV_REQUIREMENTS = "${VENV_REQUIREMENTS}"
+  def BRANCH = "${BRANCH}"
+
+  deleteDir()
+
+  echo "Deleting old VENV stored under " + VIRTUAL_ENV
+  dir(VIRTUAL_ENV) {
+    deleteDir()
+  }
+  /*
+     fuel-devops use .devops directory to store log configuration
+     we need to delete log.yaml before update to get it in current
+     version
+  */
+  withEnv(["VIRTUAL_ENV=${VIRTUAL_ENV}",
+           "DEVOPS_DB_ENGINE=${DEVOPS_DB_ENGINE}",
+           "DEVOPS_DB_NAME=${DEVOPS_DB_NAME}",
+           "BRANCH=${BRANCH}",
+           "LOG_FILE=${LOG_FILE}",
+          ]) {
+
+    if (fileExists(LOG_FILE)) {
+      echo "Deleting devops log file"
+      sh "rm ${LOG_FILE}"
+    }
+
+    if (DELETE_DB) {
+      if (fileExists(DEVOPS_DB_NAME)) {
+        sh "rm ${DEVOPS_DB_NAME}"
+      }
+    }
+
+    stage('Code checkout'){
+      sh "git clone https://github.com/openstack/fuel-ccp-tests.git ."
+      sh "git checkout ${BRANCH}"
+    }
+
+    stage('Create VENV') {
+      sh "virtualenv --no-site-packages ${VIRTUAL_ENV}"
+    }
+
+    stage('Prepare the requirements file')
+    {
+      if(VENV_REQUIREMENTS) {
+          echo "Install with custom requirements"
+          writeFile file: "${env.WORKSPACE}/venv-requirements.txt", text: VENV_REQUIREMENTS
+      } else {
+          echo "Using default requirements.txt"
+          sh "cp ${WORKSPACE}/fuel_ccp_tests/requirements.txt ${WORKSPACE}/venv-requirements.txt"
+      }
+    }
+
+    stage('Install the fuel-devops in VENV') {
+      writeFile file: "${env.WORKSPACE}/install_devops.sh", text: '''\
+        #!/bin/bash -ex
+        source ${VIRTUAL_ENV}/bin/activate
+        pip install pip --upgrade
+        pip install -r "${WORKSPACE}/venv-requirements.txt" --upgrade
+        echo "=============================="
+        pip freeze
+        echo "=============================="
+        django-admin.py syncdb --settings=devops.settings --noinput
+        django-admin.py migrate devops --settings=devops.settings --noinput
+        deactivate
+      '''.stripIndent()
+      sh "chmod 755 ${WORKSPACE}/install_devops.sh"
+      sh(returnStdout: true, script: "${env.WORKSPACE}/install_devops.sh").trim()
+    }
+  }
+
+}
