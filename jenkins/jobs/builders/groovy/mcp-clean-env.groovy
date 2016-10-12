cfd14982f5b81258dcb5feb1e00c40e2b54611f9
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-10-12 13:37:20 +0200

    Migrated mcp-clean-env to the pipelines
    This change migrates the mcp-clean-env to the
    pipelines workflow.
    
    Change-Id: Id4acbb5897c52b07b9070448653ea17842e4b033

---
+++ jenkins/jobs/builders/groovy/mcp-clean-env.groovy
@@ -0,0 +1,32 @@
+def NODE="${NODE}"
+
+node(NODE) {
+
+  def ACTION="${ACTION}"
+  def ENV_NAME="${ENV_NAME}"
+  def VIRTUAL_ENV = "/home/jenkins/venv-fuel-devops-3.0"
+  def DEVOPS_DB_ENGINE = "django.db.backends.sqlite3"
+  def DEVOPS_DB_NAME = "/home/jenkins/venv-fuel-devops-3.0.sqlite3.db"
+
+  if (!ACTION) {
+    throw new RuntimeException('No action has been specified!')
+  }
+
+  if (!ENV_NAME) {
+    throw new RuntimeException('No ' + ENV_NAME + ' has been set!')
+  }
+
+  stage('Process the environment') {
+    if(fileExists(VIRTUAL_ENV)) {
+      withEnv(["VIRTUAL_ENV=${VIRTUAL_ENV}",
+               "DEVOPS_DB_ENGINE=${DEVOPS_DB_ENGINE}",
+               "DEVOPS_DB_NAME=${DEVOPS_DB_NAME}"]) {
+        sh '''
+          bash -c "source ${VIRTUAL_ENV}/bin/activate && dos.py ${ACTION} ${ENV_NAME}"
+        '''
+      }
+    } else {
+      throw new RuntimeException('Unable to find the ' + VIRTUAL_ENV + ' dir!')
+    }
+  }
+}
