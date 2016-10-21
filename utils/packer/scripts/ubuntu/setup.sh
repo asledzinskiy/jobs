2160755b0bc6593ddc9bccf18eb41ad17e3ecb78
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-10-21 02:06:17 +0200

    Additional changes to the setup.sh script
    This change enables password-less sudo for vagrant user
    and adds it to the docker's group.
    
    Change-Id: Ic24c85b5e1711d6f8767d7a3e6bebf9f86070447

--- utils/packer/scripts/ubuntu/setup.sh
+++ utils/packer/scripts/ubuntu/setup.sh
@@ -4,6 +4,7 @@ set -eux
 
 echo "==> Setting up sudo"
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+echo "vagrant ALL=(root) NOPASSWD: ALL" > /etc/sudoers.d/10_vagrant
 
 echo "==> Configuring logging"
 touch /var/log/daemon.log
@@ -12,3 +13,6 @@ echo "daemon.* /var/log/daemon.log" >> /etc/rsyslog.d/50-default.conf
 
 echo "==> Setting default locale to en_US.UTF-8"
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+
+echo "==> Adding vagrant user to the docker group"
+gpasswd -a vagrant docker
