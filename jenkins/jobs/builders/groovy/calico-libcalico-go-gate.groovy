4807b33966b7963959572045d7933f77c5641e33
Artem Panchenko <apanchenko@mirantis.com>
2016-10-21 17:15:44 +0300

    Fix 'libcalico-go' pipeline gate job
    Align gate for 'libcalico-go' with updated
    pipeline for the project.
    
    Change-Id: I39c89b83843170b47394cba3e5181b8967de004d

--- jenkins/jobs/builders/groovy/calico-libcalico-go-gate.groovy
+++ jenkins/jobs/builders/groovy/calico-libcalico-go-gate.groovy
@@ -11,37 +11,70 @@ node('calico'){
       credentialsId = "mcp-ci-gerrit"
       branch = "mcp"
       host = "review.fuel-infra.org"
-      project = "projectcalico/libcalico-go"
+      project = "projectcalico/calico-cni"
+    }
+
+    dir("${WORKSPACE}/tmp_libcalico-go") {
+
+      gerritPatchsetCheckout {
+        credentialsId = "mcp-ci-gerrit"
+        withWipeOut = true
+      }
+
+      stage ('Running libcalico-go unittests') {
+        sh "make test-containerized"
+      }
+
     }
 
     def gitCommit = sh(returnStdout: true, script: "git -C ${WORKSPACE} rev-parse --short HEAD").trim()
-    def BUILD_ID = "mcp-${gitCommit}"
+    def CNI_BUILD = "mcp-${gitCommit}"
 
-
-    stage ('Running libcalico-go unittests') {
-      sh "make test-containerized"
+    // TODO(skulanov) GO_CONTAINER_NAME should be defined some arti image
+    stage ('Build calico-cni') {
+      def LIBCALICOGO_PATH = "${WORKSPACE}/tmp_libcalico-go"
+      gitSSHCheckout {
+        credentialsId = "mcp-ci-gerrit"
+        branch = "mcp"
+        host = "review.fuel-infra.org"
+        project = "projectcalico/libcalico-go"
+        targetDir = "${LIBCALICOGO_PATH}"
+      }
+      // TODO(apanchenko): replace `sed` by Yaml.load() -> modify map -> Yaml.dump()
+      sh """
+        sed -e '/^- name: .*\\/libcalico-go\$/{n;s/version:.*\$/repo: file:\\/\\/\\/go\\/src\\/github.com\\/projectcalico\\/libcalico-go\\n  vcs: git/;}' -i.bak glide.lock
+        grep -qP '.*repo:\\s+file:.*libcalico-go' glide.lock || { echo 1>&2 \'Repository (libcalico-go) path was not properly set in glide.lock!'; exit 1; }
+        """
+      sh "LIBCALICOGO_PATH=${LIBCALICOGO_PATH} make build-containerized"
     }
 
-
-    stage ('Build libcalico-go') {
-      sh "make build-containerized"
-    }
-
-
-    stage('Publishing libcalico-go artifacts') {
+    stage('Publishing cni artifacts') {
       dir("artifacts"){
 
-        sh "echo "
+        sh """
+          cp ${WORKSPACE}/dist/calico calico-${CNI_BUILD}
+          cp ${WORKSPACE}/dist/calico-ipam calico-ipam-${CNI_BUILD}
+        """
+
+        def calico_cni_checksum = sh(returnStdout: true, script: "sha256sum calico-${CNI_BUILD} | cut -d' ' -f1").trim()
+        def calico_cni_ipam_checksum = sh(returnStdout: true, script: "sha256sum calico-ipam-${CNI_BUILD} | cut -d' ' -f1").trim()
 
         // Save Image name ID
-        writeFile file: "lastbuild", text: "${BUILD_ID}"
+        writeFile file: "lastbuild", text: "${CNI_BUILD}"
         // Create the upload spec.
+        writeFile file: "calico-cni-${CNI_BUILD}.yaml",
+                  text: """\
+                    calico_cni_download_url: ${ARTIFACTORY_URL}/mcp/calico-cni/calico-${CNI_BUILD}
+                    calico_cni_checksum: ${calico_cni_checksum}
+                    calico_cni_ipam_download_url: ${ARTIFACTORY_URL}/mcp/calico-cni/calico-ipam-${CNI_BUILD}
+                    calico_cni_ipam_checksum: ${calico_cni_ipam_checksum}
+                  """.stripIndent()
 
         def uploadSpec = """{
             "files": [
                     {
                         "pattern": "**",
-                        "target": "projectcalico/mcp/libcalico-go/"
+                        "target": "projectcalico/mcp/calico-cni/"
                     }
                 ]
             }"""
