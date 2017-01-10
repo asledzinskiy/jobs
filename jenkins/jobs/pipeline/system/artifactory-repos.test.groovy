7462a431734bb9fec60167809e6734dc6d5b91b0
Vitaly Parakhin <vparakhin@mirantis.com>
2017-01-10 13:28:32 +0200

    Add LCM jobs for Artifactory repositories
    The following jobs are added:
    
    * verify JSON repositories definitions (on commit)
    * update repo definitions in Artifactory (on merge)
    
    Change-Id: I78dd1ebe6f810fb225f0d50939557bf5e5a23835

---
+++ jenkins/jobs/pipeline/system/artifactory-repos.test.groovy
@@ -0,0 +1,18 @@
+def ciTools = new com.mirantis.mcp.Common()
+def gitTools = new com.mirantis.mcp.Git()
+
+node('tools') {
+
+    stage('Code checkout') {
+        gitTools.gerritPatchsetCheckout {
+            credentialsId = "mcp-ci-gerrit"
+        }
+    }
+
+    withEnv(["VENV_PATH=${env.WORKSPACE}/.tox/artifactory-repos"]) {
+        stage('Validate repository configs') {
+            ciTools.runTox("artifactory-repos-verify")
+        }
+
+    }
+}
