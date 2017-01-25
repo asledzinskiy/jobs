feeffba1f83303e557d118abc9798edd54ac1452
Sergey Kulanov <skulanov@mirantis.com>
2017-01-25 13:21:47 +0200

    [calico] Add support for running libcalico-go on k8s cluster
    Add calico-libcalico-go-test-pipeline job which runs on k8s slave
    in non-voting mode
    
    Change-Id: I38813dc387f8e7d26efbd7db6dba8e1ce64d2587

--- jenkins/jobs/pipeline/calico/calico-libcalico-go.groovy
+++ jenkins/jobs/pipeline/calico/calico-libcalico-go.groovy
@@ -1,8 +1,32 @@
 def gitTools = new com.mirantis.mcp.Git()
-node('calico'){
+def common = new com.mirantis.mcp.Common()
 
+jenkinsSlaveImg = 'docker-prod-virtual.docker.mirantis.net/mirantis/jenkins-slave-images/projectcalico-debian-slave-20161223134732:latest'
+jnlpSlaveImg = 'docker-prod-virtual.docker.mirantis.net/mirantis/jenkins-slave-images/jnlp-slave:latest'
+
+try {
+  def artifacts = []
+  if (env.RUN_ON_K8S == 'true') {
+    // run job on k8s cluster
+    artifacts = common.runOnKubernetes([
+      function : this.&buildLibcalicoGo,
+      jnlpImg  : jnlpSlaveImg,
+      slaveImg : jenkinsSlaveImg
+    ])
+  } else {
+    // run job on HW node with label
+    node ('calico'){
+      artifacts = buildLibcalicoGo()
+    }
+  }
+}
+catch(err) {
+  echo "Failed: ${err}"
+  currentBuild.result = 'FAILURE'
+}
+
+def buildLibcalicoGo(){
   try {
-
     def HOST = env.GERRIT_HOST
 
     stage ('Checkout libcalico-go'){
