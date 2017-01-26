44861dd3c7e34e7874b3bf8a3beadf899aadf36c
Ryabin Sergey <sryabin@mirantis.com>
2017-01-26 21:50:53 +0400

    Get information about what file was changed
    Change-Id: I53e44d77a21c1494e6eaadf34ecc535ac342a366

--- jenkins/jobs/pipeline/ccp/ccp-demo-trigger.groovy
+++ jenkins/jobs/pipeline/ccp/ccp-demo-trigger.groovy
@@ -1,3 +1,44 @@
+def parseGerritURL(String gerritURL, String what) {
+    assert(gerritURL != null)
+
+    /*
+       Looks like it's a bug in a groovy pipeline
+       'ssh://foobar:xxx/'.replaceFirst(/^ssh:/, 'http:') return foobar:xxx
+    */
+    return sh(
+            script: "python -c 'from urlparse import urlparse; print urlparse(\"" + gerritURL + "\")." + what + "'",
+            returnStdout: true
+    ).trim()
+}
+
+
 node {
-  sh 'env'
+    def confGerritHost = parseGerritURL(CONF_GERRIT_URL, 'hostname')
+    def confGerritPath = parseGerritURL(CONF_GERRIT_URL, 'path')
+
+
+    def targetDir = "conf-repo-" + env.BUILD_ID
+    def changedFile = null
+
+    stage('Checkout env configuration') {
+      def gitTools = new com.mirantis.mcp.Git()
+      gitTools.gitSSHCheckout ([
+        credentialsId : env.CONF_GERRIT_CREDENTIAL_ID,
+        branch : "master",
+        host : confGerritHost,
+        project : confGerritPath,
+        targetDir: targetDir
+      ])
+      def changedFile = sh(
+        script: """
+cd $targetDir
+git show `git rev-parse HEAD` | grep -m 1 -oE '\\/(config|version)s\\.yaml\$'
+""",
+        returnStdout: true
+      )
+
+    }
+    stage('Apply changes') {
+      echo changedFile
+    }
 }
